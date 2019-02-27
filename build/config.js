b63cedad5a5d1b4fc70729f1777c0b970f9b5535
Martin Guillon <martin.guillon@akylas.fr>
2019-02-27 15:14:35 +0100

    build: 2.0.5

--- build/config.js
+++ build/config.js
@@ -18,87 +18,88 @@ const banner = (name, version) => `
  */
 `
 const resolveVue = p => {
-    return path.resolve(process.cwd(), 'node_modules', 'vue/src/', p) + '/'
+  return path.resolve(process.cwd(), 'node_modules', 'vue/src/', p) + '/'
 }
 const aliases = {
-    vue: resolveVue('core/index'),
-    compiler: resolveVue('compiler'),
-    web: resolveVue('platforms/web'),
-    core: resolveVue('core'),
-    shared: resolveVue('shared'),
-    sfc: resolveVue('sfc'),
-    //he: path.resolve(__dirname, 'node_modules', 'he', 'he')
-    he: path.resolve(__dirname, '..', 'platform/nativescript/util/entity-decoder')
+  vue: resolveVue('core/index'),
+  compiler: resolveVue('compiler'),
+  web: resolveVue('platforms/web'),
+  core: resolveVue('core'),
+  shared: resolveVue('shared'),
+  sfc: resolveVue('sfc'),
+  //he: path.resolve(__dirname, 'node_modules', 'he', 'he')
+  he: path.resolve(__dirname, '..', 'platform/nativescript/util/entity-decoder')
 }
 
+const compilerExternals = Object.keys(
+  require('../packages/akylas-nativescript-vue-template-compiler/package.json')
+    .dependencies
+)
 const builds = {
-    'nativescript-vue': {
-        entry: './platform/nativescript/framework.js',
-        dest: './dist/index.js',
-        moduleName: 'NativeScript-Vue',
-        banner: banner('NativeScript-Vue'),
-        external(id) {
-            return /tns-core-modules/.test(id) || /weex/.test(id)
-        }
-    },
-    'nativescript-vue-template-compiler': {
-        entry: './platform/nativescript/compiler.js',
-        dest: './packages/nativescript-vue-template-compiler/index.js',
-        moduleName: 'NativeScript-Vue-Template-Compiler',
-        banner: banner('NativeScript-Vue-Template-Compiler'),
-        external(id) {
-            return /tns-core-modules/.test(id) || /weex/.test(id)
-        }
-        // external: Object.keys(require('../packages/nativescript-vue-template-compiler/package.json').dependencies)
+  'akylas-nativescript-vue': {
+    entry: './platform/nativescript/framework.js',
+    dest: './dist/index.js',
+    moduleName: 'Akylas-NativeScript-Vue',
+    banner: banner('Akylas-NativeScript-Vue'),
+    external(id) {
+      return /tns-core-modules/.test(id) || /weex/.test(id)
     }
+  },
+  'akylas-nativescript-vue-template-compiler': {
+    entry: './platform/nativescript/compiler.js',
+    dest: './packages/akylas-nativescript-vue-template-compiler/index.js',
+    moduleName: 'Akylas-NativeScript-Vue-Template-Compiler',
+    banner: banner('Akylas-NativeScript-Vue-Template-Compiler'),
+    external(id) {
+      return /tns-core-modules/.test(id) || compilerExternals.indexOf(id) !== -1
+    }
+  }
 }
 
+const genConfig = name => {
+  const opts = builds[name]
+  const config = {
+    input: opts.entry,
+    external: opts.external,
+    output: {
+      file: opts.dest,
+      format: opts.format || 'cjs',
+      banner: opts.banner,
+      name: opts.moduleName
+    },
+    treeshake: {
+      pureExternalModules: id => id.startsWith('weex')
+    },
+    watch: {
+      chokidar: false
+    },
+    plugins: [
+      replace({
+        __WEEX__: false,
+        __VERSION__: VueVersion,
+        'process.env.NODE_ENV': "'development'",
+        'let _isServer': 'let _isServer = false',
+        'process.env.VUE_VERSION': `'${VueVersion}'`,
+        'process.env.NS_VUE_VERSION': `'${NSVueVersion}'`
+      }),
+      flow(),
+      buble(),
+      alias(aliases),
+      resolve(),
+      commonjs()
+    ]
+  }
 
-const genConfig = (name) => {
-    const opts = builds[name]
-    console.log('genConfig', name);
-    const config = {
-        input: opts.entry,
-        external: opts.external,
-        output: {
-            file: opts.dest,
-            format: opts.format || 'cjs',
-            banner: opts.banner,
-            name: opts.moduleName
-        },
-        treeshake: {
-            pureExternalModules: id => id.startsWith('weex')
-        },
-        watch: {
-            chokidar: false
-        },
-        plugins: [
-            replace({
-                __WEEX__: false,
-                __VERSION__: VueVersion,
-                'process.env.NODE_ENV': "'development'",
-                'let _isServer': 'let _isServer = false',
-                'process.env.VUE_VERSION': `'${VueVersion}'`,
-                'process.env.NS_VUE_VERSION': `'${NSVueVersion}'`
-            }),
-            flow(),
-            buble(),
-            alias(aliases),
-            resolve(),
-            commonjs(),
-        ],
-    }
+  Object.defineProperty(config, '_name', {
+    enumerable: false,
+    value: name
+  })
 
-    Object.defineProperty(config, '_name', {
-        enumerable: false,
-        value: name
-    })
-
-    return config;
+  return config
 }
 
 if (process.env.TARGET) {
-    module.exports = genConfig(process.env.TARGET)
+  module.exports = genConfig(process.env.TARGET)
 } else {
-    module.exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
+  module.exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
 }
