2c6504bcea22e6055d9999a47bae963203df01ff
Martin Guillon <martin.guillon@akylas.fr>
2020-01-23 13:19:57 +0100

    fix: show error stack

--- platform/nativescript/element-registry.js
+++ platform/nativescript/element-registry.js
@@ -65,7 +65,9 @@ export function getViewClass(elementName) {
   try {
     return entry.resolver()
   } catch (e) {
-    throw new TypeError(`Could not load view for: ${elementName}. ${e}`)
+    throw new TypeError(
+      `Could not load view for: ${elementName}. ${e} ${e.stack}`
+    )
   }
 }
 
